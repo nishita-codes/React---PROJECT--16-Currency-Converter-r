43a4f27761d9ada48a46e778f2b212be4ccff786
Nishita-codes <nishitapatidar03@gmail.com>
2025-03-21 23:45:48 +0530

    added files

--- react_currency/src/App.jsx
+++ react_currency/src/App.jsx
@@ -1,12 +1,44 @@
 const App = () =>{
   return <section className = "currency-Converter">
-    <hr />
-    <div classNmae="currency-div">
+    
+    <div className="currency-div">
       <h1>Currency Converter</h1>
+      <hr id="horizontal" />
       <div>
         <label htmlFor="currency_amount">Amount:
           <input type="number"  id="currency_amount"/>
         </label>
+      </div>
+      <div className="currency-selector">
+        <div>
+          <label>
+            From:
+            <select name="" id= "">
+              <option value="USD">USD</option>
+              <option value="EUR">EUR</option>
+              <option value="INR">INR</option>
+              <option value="GBP">GBP </option>
+              <option value="AUD">AUD</option>
+              <option value="JEP">JEP</option>
+              <option value="MMK">MMK</option>
+            </select>
+          </label>
+        </div>
+
+        <div>
+          <label>
+            To:
+            <select name="" id= "">
+            <option value="INR">INR</option>
+              <option value="USD">USD</option>
+              <option value="EUR">EUR</option>
+              <option value="GBP">GBP </option>
+              <option value="AUD">AUD</option>
+              <option value="JEP">JEP</option>
+              <option value="MMK">MMK</option>
+            </select>
+          </label>
+        </div>
       </div>
     </div>
   </section>
