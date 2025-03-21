492e28a2952c824f1b2218354a7fbd3ef687f340
Nishita-codes <nishitapatidar03@gmail.com>
2025-03-21 23:06:04 +0530

    added files

---
+++ api/postApi.jsx
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL :" https://v6.exchangerate-api.com/v6/c9770dd5bb4adb71ab6c6857/latest/USD"
+});
+
+export const currencyConverter = (fromCurrency, toCurrency ,amount) =>{
+    return api.get(`/pair/${fromCurrency}/${toCurrency}/${amount}`);
+};
