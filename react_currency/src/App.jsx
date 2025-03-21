25050f293b9825631c36f1ab5f0aabb6562fc81e
Nishita-codes <nishitapatidar03@gmail.com>
2025-03-21 23:23:48 +0530

    added files

--- react_currency/src/App.jsx
+++ react_currency/src/App.jsx
@@ -1,4 +1,14 @@
 const App = () =>{
-  return <h1>hello Currency</h1>
+  return <section className = "currency-Converter">
+    <hr />
+    <div classNmae="currency-div">
+      <h1>Currency Converter</h1>
+      <div>
+        <label htmlFor="currency_amount">Amount:
+          <input type="number"  id="currency_amount"/>
+        </label>
+      </div>
+    </div>
+  </section>
 };
 export default App;
